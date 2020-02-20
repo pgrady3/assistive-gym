288c82309d55ba111d5c8397745d71ff3548902b
Patrick Grady <hmfpa392@ameritech.net>
2020-02-20 13:05:34 -0500

    Cleaned manual

--- examples/manual.py
+++ examples/manual.py
@@ -2,21 +2,12 @@ import gym, assistive_gym
 import pybullet as p
 import numpy as np
 from p_utils import *
-#from scipy.spatial.transform import Slerp
-#from scipy.spatial.transform import Rotation as R
 from pyquaternion import Quaternion
 
 class Waypoint:
     def __init__(self, x, theta): 
         self.x = np.array(x)
         self.theta = p.getQuaternionFromEuler(np.array(theta))
-
-def my_slerp(p0, p1, t):
-    r0 = R.from_quat(p0)
-    r1 = R.from_quat(p1)
-    slerp = Slerp([0, 1], np.array([theta, wpt_theta]))
-    return slerp(t)
-
 
 env = gym.make('DrinkingJaco-v0')
 env.render()
@@ -54,16 +45,11 @@ for rollout in range(10):
         q1 = Quaternion(wpt_theta)
         q  = Quaternion.slerp(q0, q1, 0.15) # Rotate 120 degrees (2 * pi / 3)
 
-        #print('end ', q1.elements)
-        #print('goal', q.elements)
-
         if dist_to_goal < 0.04 and wpt_idx < len(waypoints)-1:
             wpt_idx = wpt_idx + 1
             old_wpt_theta = waypoints[wpt_idx-1].theta
 
-
-        target_joint_positions = p.calculateInverseKinematics(env.robot, 8, interp_x, q.elements)
-        #target_joint_positions = p.calculateInverseKinematics(env.robot, 8, target_pos, wpt_theta)
+        target_joint_positions = p.calculateInverseKinematics(env.robot, 8, interp_x, q.elements) # Go to interpolated positions
         target_joint_positions = target_joint_positions[:7]
 
         # Get the joint positions (angles) of the robot arm
@@ -75,7 +61,7 @@ for rollout in range(10):
         if np.linalg.norm(joint_action) > 3:
             joint_action = joint_action / np.linalg.norm(joint_action) * 3
 
-        print(np.linalg.norm(joint_action))
+        #print(np.linalg.norm(joint_action))
 
         observation, reward, done, info = env.step(joint_action)
         total_reward += reward
